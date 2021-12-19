cd4627ed6ecd1f27c38f3952a79388442ccf63e6
Luis Preciado <luisapreciado99@gmail.com>
2021-12-18 19:58:48 -0500

    Changed port configuration in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const cts = require("./constants");
+const port = process.env.PORT || cts.PORT;
 
-app.listen(cts.PORT, () => {
-  console.log("listening on port: " + cts.PORT);
+app.listen(port, () => {
+  console.log("listening on port: " + port);
 });
